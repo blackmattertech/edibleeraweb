f197ce001695497ff5e3a920b948a25a3e8fb153
Mukesh <info@blackmattertech.com>
2026-06-16 17:00:57 +0530

    refresh issue

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import { BrowserRouter } from 'react-router-dom'
+import { ScrollToTop } from './components/ScrollToTop'
 import './index.css'
 import App from './App.tsx'
 
@@ -9,6 +10,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
       <BrowserRouter>
+        <ScrollToTop />
         <App />
       </BrowserRouter>
     </HelmetProvider>
